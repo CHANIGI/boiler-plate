90714f9da28fc8a1029c645fb9e8a384baa90cad
CHANIGI <dmdkd4687@naver.com>
2022-12-13 22:45:53 +0900

    Auth route 구현

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const {User} = require('./models/User')
 const bodyParser = require('body-parser')
 const config = require('./config/key')
 const cookieParser = require('cookie-parser')
+const {auth} = require('./middleware/auth')
 
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: true}))
@@ -22,7 +23,7 @@ mongoose.set('strictQuery', false)
 
 app.get('/', (req, res) => res.send('Hello  world'))
 
-app.post('/register', (req, res) => {
+app.post('/api/register', (req, res) => {
   //회원 가입시 필요한 정보들을 client에서 가져오면
   //database에 넣어주기
   const user = new User(req.body)
@@ -34,7 +35,7 @@ app.post('/register', (req, res) => {
     })
   })
 })
-app.post('/login', (req, res) => {
+app.post('/api/login', (req, res) => {
   //요청된 이메일을 데이터베이스에서 있는지 찾음
   User.findOne({email:req.body.email}, (err, user) => {
     if(!user){
@@ -54,6 +55,19 @@ app.post('/login', (req, res) => {
         .json({loginSuccess: true, userID: user._id})
       })
     })
+  })
+})
+
+app.get('/api/users/auth', auth, (req, res) => {
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0? false: true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user,lastname,
+    role: req.user.role,
+    image: req.user.image
   })
 })
 
