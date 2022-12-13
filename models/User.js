9c89020ce35f5a0d0753bf148df1688085a88787
CHANIGI <dmdkd4687@naver.com>
2022-12-13 22:23:29 +0900

    로그인 기능 구현

--- models/User.js
+++ models/User.js
@@ -1,6 +1,9 @@
 'use strict'
 
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const saltRounds = 10
+const jwt = require('jsonwebtoken')
 
 const userSchema = mongoose.Schema({
   name: {
@@ -14,7 +17,7 @@ const userSchema = mongoose.Schema({
   },
   password:{
     type: String,
-    maxlength: 50
+    maxlength: 100
   },
   lastname: {
     type: String,
@@ -32,6 +35,40 @@ const userSchema = mongoose.Schema({
     type: Number
   }
 })
+userSchema.pre('save', function(next){
+   //비밀번호 암호화
+  var user = this
+  if(user.isModified('password')){
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+      if(err) return next(err)
+      bcrypt.hash(user.password, salt, function(err, hash){
+       if(err) return next(err)
+       user.password = hash
+       next()
+       })
+     })
+   } else {
+      next()
+   }
+})
+
+userSchema.methods.comparePassword = function(plainPassword, cb){
+  bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+    if(err) return cb(err)
+    cb(null, isMatch)
+  })
+}
+userSchema.methods.generateToken = function(cb){
+  var user = this
+  //jsonwebToken을 이용해서 token을 생성하기
+  var token = jwt.sign(user._id.toHexString(), 'secretToken')
+  user.token = token
+  user.save(function(err, user){
+    if(err) return cb(err)
+    cb(null, user)
+  })
+ 
+}
 
 const User = mongoose.model('User', userSchema)
 module.exports = {User}
